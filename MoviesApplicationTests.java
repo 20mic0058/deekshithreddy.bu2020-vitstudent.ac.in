e205adf81e96f4567f7f62c14fe7e75f67030d74
20mic0058 <95293467+20mic0058@users.noreply.github.com>
2023-06-30 16:52:46 +0530

    Add files via upload

---
+++ MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.deekshith.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
